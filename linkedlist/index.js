d49bd402086ad61c4242c2449a47b40890ba7e77
Rianna <rianna.cleary@gmail.com>
2020-05-15 13:34:10 -0400

    updated linked list class

--- linkedlist/index.js
+++ linkedlist/index.js
@@ -10,7 +10,13 @@ class Node {
 }
 
 class LinkedList {
+  constructor() {
+    this.head = null;
+  }
 
+  insertFirst(data) {
+    this.head = new Node(data, this.head);
+  }
 }
 
 module.exports = { Node, LinkedList };
