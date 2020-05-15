7a7a742e5485832cb744f1032505b375a9d90466
Rianna <rianna.cleary@gmail.com>
2020-05-15 16:29:53 -0400

    removeFirst done

--- linkedlist/index.js
+++ linkedlist/index.js
@@ -50,6 +50,13 @@ class LinkedList {
   clear() {
     this.head = null;
   }
+
+  removeFirst() {
+    if (!this.head) {
+      return;
+    }
+    this.head = this.head.next
+  }
 }
 
 module.exports = { Node, LinkedList };
