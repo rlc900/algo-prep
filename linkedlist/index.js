3ce3a6fb03537eea7ae37610de2b2d33ad8b0e7a
Rianna <rianna.cleary@gmail.com>
2020-05-18 13:21:10 -0400

    finished removedLast()

--- linkedlist/index.js
+++ linkedlist/index.js
@@ -57,6 +57,27 @@ class LinkedList {
     }
     this.head = this.head.next
   }
+
+  removeLast() {
+
+
+
+    if (!this.head) {
+      return;
+    }
+
+    if (!this.head.next) {
+      this.head = null;
+      return;
+    }
+    let prev = this.head;
+    let node = this.head.next;
+      while (node.next) {
+      prev = node
+      node = node.next
+    }
+    prev.next = null;
+  }
 }
 
 module.exports = { Node, LinkedList };
