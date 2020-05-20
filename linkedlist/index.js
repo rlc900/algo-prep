caf6f0521335430e28a565b216a6241a3a454cbd
Rianna <rianna.cleary@gmail.com>
2020-05-20 14:03:23 -0400

    insertAt() finished

--- linkedlist/index.js
+++ linkedlist/index.js
@@ -106,7 +106,7 @@ class LinkedList {
     if (!this.head) {
       return;
     }
-    
+
     if (index === 0) {
       this.head = this.head.next;
       return;
@@ -116,6 +116,28 @@ class LinkedList {
         return;
       }
     prev.next = prev.next.next;
+  }
+
+  insertAt(data, index) {
+
+    // if we try to insert a ele into list that is completely empty
+    if (!this.head) {
+      this.head = new Node(data)
+      return;
+    }
+    if (index === 0) {
+      this.head = new Node(data, this.head);
+      return;
+    }
+    // use  getAt() to get access to prev node in the chain (index - 1)
+    // make sure prev = new ele &  new ele = old one
+
+    // if index is out of bounds ADD TO END OF LIST
+    const prev = this.getAt(index - 1) || this.getLast()
+    const node  = new Node(data, prev.next)
+    prev.next = node
+
+
   }
 }
 
