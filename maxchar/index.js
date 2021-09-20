2fe0b7360237f3f4628654ee09b18a82d51355ee
Rianna <rianna.cleary@gmail.com>
2021-09-20 15:38:24 -0400

    deleted maxchar

--- maxchar/index.js
+++ maxchar/index.js
@@ -7,25 +7,7 @@
 
 function maxChar(str) {
 
-  const chars = {};
-  let max = 0;
-  let maxChar = '';
-
-  for (let char of str) {
-    if (!chars[char]) {
-      chars[char] = 1;
-    } else {
-      chars[char]++;
-    }
-  }
-
-  for (let char in chars) {
-    if (chars[char] > max) {
-      max = chars[char];
-      maxChar = char
-    }
-  }
-  return maxChar;
+ 
 };
 
 module.exports = maxChar;
