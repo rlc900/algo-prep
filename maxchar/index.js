3247424e59e217135d74383c8a8ff57f295fa2ee
Rianna <rianna.cleary@gmail.com>
2021-09-21 11:18:40 -0400

    completed maxchar

--- maxchar/index.js
+++ maxchar/index.js
@@ -6,8 +6,32 @@
 // maxChar("apple 1231111") === "1"
 
 function maxChar(str) {
-
- 
-};
+        // create helper function that creates an object with the character count of each letter in the string.
+        // iterate through that object and have a conditional to check which key has the most characters
+        // return the character
+        let max = 0;
+        let maxChar = '';
+        const stringToCharMap = createCharMap(str)
+        
+        for (let char in stringToCharMap ) {
+          if (stringToCharMap[char] > max) {
+            max = stringToCharMap[char]
+            maxChar = char;
+          } 
+        }
+        return maxChar;
+        };
+        
+        function createCharMap(str) {
+          const charMap = {};
+          for (let letter of str) {
+           if (charMap[letter]) {
+             charMap[letter]++
+           } else {
+             charMap[letter] = 1
+           }
+          }
+          return charMap
+        };
 
 module.exports = maxChar;
